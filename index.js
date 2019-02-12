bb2dbe042b8dd63a12fa5ca68af40b7f495e752c
AJ Guiles <ashiguiles@gmail.com>
2019-02-12 03:54:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var cart = [];
 
 function getCart() {
- return cart;
+return cart;
 }
 
 function setCart(c) {
@@ -10,21 +10,74 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
-}
+  
+  var price = Math.floor(Math.random()*100) +1;
+  var itemAndPrice = { [item]: price}
+  
+cart.push(itemAndPrice);
+console.log(`${item} has been added to your cart.`)
+
+return cart;
+ }
+
+
+addToCart('apples')
+addToCart('bananas')
+addToCart('daikon')
 
 function viewCart() {
-  // write your code here
+  var listOfItems = [];
+  if (cart.length === 0) {
+    return 'Your shopping Cart is empty.'
 }
 
-function total() {
-  // write your code here
+
+for (let i = 0; i < cart.length; i++ ) 
+{
+    var cartList = cart[i];
+    var key = Object.keys(cartList)[0];
+    var price = cartList[key];
+    listOfItems.push(`${key} at $${price}`) 
+  }
+  
+  if (cart.length === 1) {return `In your cart, you have ${listOfItems}.` }
+  else if (cart.lenght === 2 ) { return 'In your cart, you have ${listOfItems[0]} and ${listOfItems[1]}.' 
+} else  {
+  return  `In your cart, you have ${listOfItems.slice(0,-1).join(', ')}, and ${listOfItems[listOfItems.length-1]}.`
 }
+}
+
+//review later 
+function total() {
+  var totCharge= 0;
+  for (var i = 0; i < cart.length; i++) {
+    var cartList = cart[i];
+    var key = Object.keys(cartList)[0];
+    var price = object[key];
+    sum += price;
+}
+return sum;
+
+}
+
+total()
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i = 0; i < cart.length; i++) {
+    var cartList = cart[i];
+    var key = Object.keys(cartList)[0];
+    if (cartList.property(item)) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  console.log("That item is not in your cart.");
 }
+removeFromCart('bananas')
+
+
 
 function placeOrder(cardNumber) {
   // write your code here
 }
+
